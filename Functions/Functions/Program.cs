b988e957eb06504b2a679f8b29325878b3942479
Damyan Maxwell <saintnyte@gmail.com>
2024-11-22 10:52:55 +0100

    af

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Functions
+﻿namespace readlines
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+
+            program.Run();
+
+            // Variable: program
+            // Type: class
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Did it work?");
+            Vraag1();
+        }
+
+        internal void Vraag1()
+        {
+            Console.WriteLine("Nah bro it worked");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
         }
     }
 }
