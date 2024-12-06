ea21a9c99fedbdfeb33ab2f00a089d7d875e22f1
Damyan Maxwell <saintnyte@gmail.com>
2024-12-06 09:40:39 +0100

    af

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -20,7 +20,63 @@
 
         internal void Vraag1()
         {
-            Console.WriteLine("Nah bro it worked");
+            Console.WriteLine("Nah bro it worked.");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Ok dude. Now is not the time to make jokes. Did it work or not?");
+            Vraag2();
+        }
+
+        internal void Vraag2()
+        {
+            Console.WriteLine("Yeah yeah yeah stop whining. The stupid thing worked.");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Are you sure?");
+            Vraag3();
+        }
+
+        internal void Vraag3()
+        {
+            Console.WriteLine("Yes dude!");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Can there be any more modifications added?");
+            Vraag4();
+        }
+
+        internal void Vraag4()
+        {
+            Console.WriteLine("Yeah, but it's gonna cost ya");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("How much?");
+            Vraag5();
+        }
+
+        internal void Vraag5()
+        {
+            Console.WriteLine("Five thousand lil bro");
             string antwoord = Console.ReadLine();
 
             Console.WriteLine(antwoord);
